9ad0ce817e79a2b95151ea5ff728bb28cfa04f14
Veton Matoshi <veton-matoshi@hotmail.de>
2022-07-14 18:32:50 +0200

    online_terms_of_service_unfairness_level: Edited scripts to finetune a model

--- experiments/run_online_terms_of_service_unfairness_level.py
+++ experiments/run_online_terms_of_service_unfairness_level.py
@@ -11,12 +11,12 @@ from typing import Optional
 import pandas as pd
 
 import datasets
+from helper import reduce_size, compute_metrics_multi_class
 from datasets import load_dataset, Dataset
 from sklearn.metrics import f1_score
 import numpy as np
 import glob
 import shutil
-from bcd_helper import get_label_dict, create_dataset
 
 import transformers
 from transformers import (
@@ -93,6 +93,21 @@ class DataTrainingArguments:
             "value if set."
         },
     )
+    language:Optional[str] = field(
+        default='all_languages',
+        metadata={
+            "help": "For choosin the language "
+            "value if set."
+        },
+    )
+    running_mode:Optional[str] = field(
+        default='default',
+        metadata={
+            "help": "If set true only a small portion of the original dataset will be used for fast experiments"
+            "value if set."
+        },
+    )
+
     server_ip: Optional[str] = field(default=None, metadata={"help": "For distant debugging."})
     server_port: Optional[str] = field(default=None, metadata={"help": "For distant debugging."})
 
@@ -208,7 +223,10 @@ def main():
     def prepare_dataset(dataset):
         dataset =pd.DataFrame(dataset)
         dataset['label']=dataset['unfairness_level']
-        dataset = dataset[['sentence','label']]
+        dataset = dataset[dataset.label!='untagged']
+        if data_args.language!='all_languages':
+            dataset = dataset[dataset.language==data_args.language]
+        #dataset = dataset[['sentence','label']]
         dataset = Dataset.from_pandas(dataset)
         return dataset
 
@@ -228,9 +246,22 @@ def main():
     eval_dataset =prepare_dataset(eval_dataset)
     predict_dataset =prepare_dataset(predict_dataset)
 
+
     # Labels
     label_list = sorted(list(set(train_dataset['label'])))
     num_labels = len(label_list)
+
+    label2id = dict()
+    id2label = dict()
+
+    for n,l in enumerate(label_list):
+        label2id[l]=n
+        id2label[n]=l
+
+    if data_args.running_mode=='experimental':
+        train_dataset = reduce_size(train_dataset, 1000)
+        eval_dataset = reduce_size(eval_dataset,200)
+        predict_dataset = reduce_size(predict_dataset,100)
 
     # Load pretrained model and tokenizer
     # In distributed training, the .from_pretrained methods guarantee that only one local process can concurrently
@@ -238,7 +269,7 @@ def main():
     config = AutoConfig.from_pretrained(
         model_args.config_name if model_args.config_name else model_args.model_name_or_path,
         num_labels=num_labels,
-        finetuning_task="online_terms_of_service_unfairness_category",
+        finetuning_task=data_args.language+"_online_terms_of_service_unfairness_level",
         cache_dir=model_args.cache_dir,
         revision=model_args.model_revision,
         use_auth_token=True if model_args.use_auth_token else None,
@@ -320,15 +351,6 @@ def main():
                 desc="Running tokenizer on prediction dataset",
             )
 
-    # You can define your custom compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
-    # predictions and label_ids field) and has to return a dictionary string to float.
-    def compute_metrics(p: EvalPrediction):
-        logits = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
-        preds = np.argmax(logits, axis=1)
-        macro_f1 = f1_score(y_true=p.label_ids, y_pred=preds, average='macro', zero_division=0)
-        micro_f1 = f1_score(y_true=p.label_ids, y_pred=preds, average='micro', zero_division=0)
-        return {'macro-f1': macro_f1, 'micro-f1': micro_f1}
-
     # Data collator will default to DataCollatorWithPadding, so we change it if we already did the padding.
     if data_args.pad_to_max_length:
         data_collator = default_data_collator
@@ -343,7 +365,7 @@ def main():
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
-        compute_metrics=compute_metrics,
+        compute_metrics=compute_metrics_multi_class,
         tokenizer=tokenizer,
         data_collator=data_collator,
         callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
@@ -399,6 +421,22 @@ def main():
                 for index, pred_list in enumerate(predictions):
                     pred_line = '\t'.join([f'{pred:.5f}' for pred in pred_list])
                     writer.write(f"{index}\t{pred_line}\n")
+
+
+        predict_dataset_df = pd.DataFrame(predict_dataset)
+
+        preds = np.argmax(predictions, axis=-1)
+
+        output = list(zip(predict_dataset_df.sentence.tolist(),labels,preds,predictions))
+        output = pd.DataFrame(output, columns = ['sentence','reference','predictions','logits'])
+        
+        output['predictions_as_label']=output.predictions.apply(lambda x: id2label[x])
+        output['reference_as_label']=output.reference.apply(lambda x: id2label[x])
+        
+        output_predict_file_new_json = os.path.join(training_args.output_dir, "test_predictions_clean.json")
+        output_predict_file_new_csv = os.path.join(training_args.output_dir, "test_predictions_clean.csv")
+        output.to_json(output_predict_file_new_json, orient='records', force_ascii=False)
+        output.to_csv(output_predict_file_new_csv)
 
 
     # Clean up checkpoints
