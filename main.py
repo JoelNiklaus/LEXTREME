9450aec5dfd27f98ec7b3fa734519cb943a37095
Veton Matoshi <veton-matoshi@hotmail.de>
2022-08-09 17:54:36 +0200

    Removed some bugs. Now there are more possibilities to choose different configurations

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def generate_command(**data):
 
     time_now = datetime.datetime.now().isoformat().split(':')[:1][0]
 
-    command_template = 'CUDA_VISIBLE_DEVICES={GPU_NUMBER} python ../experiments/{CODE} --model_name_or_path {MODEL_NAME} --do_lower_case {LOWER_CASE}  --output_dir logs/{LANGUAGE}'+'_'+'{TASK}/{MODEL_NAME}/seed_{SEED} --do_train --do_eval --do_pred --overwrite_output_dir --load_best_model_at_end --metric_for_best_model {METRIC_FOR_BEST_MODEL} --greater_is_better True --evaluation_strategy epoch --save_strategy epoch --save_total_limit 5 --num_train_epochs {NUM_TRAIN_EPOCHS} --learning_rate {LEARNING_RATE} --per_device_train_batch_size {BATCH_SIZE} --per_device_eval_batch_size {BATCH_SIZE} --seed {SEED} --fp16 --fp16_full_eval --gradient_accumulation_steps {ACCUMULATION_STEPS} --eval_accumulation_steps {ACCUMULATION_STEPS} --running_mode {RUNNING_MODE}'
+    command_template = 'CUDA_VISIBLE_DEVICES={GPU_NUMBER} python ../experiments/{CODE} --model_name_or_path {MODEL_NAME} --do_lower_case {LOWER_CASE}  --output_dir logs/'+'{TASK}/{MODEL_NAME}/seed_{SEED} --do_train --do_eval --do_pred --overwrite_output_dir --load_best_model_at_end --metric_for_best_model {METRIC_FOR_BEST_MODEL} --greater_is_better True --evaluation_strategy epoch --save_strategy epoch --save_total_limit 5 --num_train_epochs {NUM_TRAIN_EPOCHS} --learning_rate {LEARNING_RATE} --per_device_train_batch_size {BATCH_SIZE} --per_device_eval_batch_size {BATCH_SIZE} --seed {SEED} --fp16 --fp16_full_eval --gradient_accumulation_steps {ACCUMULATION_STEPS} --eval_accumulation_steps {ACCUMULATION_STEPS} --running_mode {RUNNING_MODE}'
 
     
     final_command = command_template.format(GPU_NUMBER=data["gpu_number"],MODEL_NAME=data["model_name"],LOWER_CASE=data["lower_case"],TASK=data["task"],SEED=data["seed"],NUM_TRAIN_EPOCHS=data["num_train_epochs"],BATCH_SIZE=data["batch_size"],ACCUMULATION_STEPS=data["accumulation_steps"],LANGUAGE=data["language"],RUNNING_MODE=data["running_mode"],LEARNING_RATE=data["learning_rate"],CODE=data["code"],METRIC_FOR_BEST_MODEL=data["metric_for_best_model"]) # I must be careful, it another stragey might require greater_is_better = False
@@ -68,7 +68,7 @@ def run_in_parallel(commands_to_run):
         pool = Pool(processes=len(commands_to_run))
         pool.map(run_script, commands_to_run)
 
-def run_experiment(language_model_type='all',running_mode='default', task='all',list_of_seeds=[1,2,3,4],lower_case=True,num_train_epochs=20,batch_size=10,accumulation_steps=1,language='all_languages',learning_rate=1e-5,gpu_number=None):
+def run_experiment(language_model_type='all',running_mode='default', task='all',list_of_seeds=None,lower_case=True,num_train_epochs=20,batch_size=10,accumulation_steps=1,language='all_languages',learning_rate=1e-5,gpu_number=None):
 
 
     if gpu_number is None:
@@ -81,13 +81,15 @@ def run_experiment(language_model_type='all',running_mode='default', task='all',
     os.environ['CUDA_DEVICE_ORDER']='PCI_BUS_ID'
     os.environ['CUDA_VISIBLE_DEVICES'] = ','.join([str(x) for x in gpu_number])
 
-    if list_of_seeds is None:
+    if type(list_of_seeds)==list:
+        list_of_seeds = [int(s) for s in list_of_seeds if s]
+    elif list_of_seeds is None:
         list_of_seeds=[1,2,3,4]
-    else:
+    elif type(list_of_seeds)==str:
         list_of_seeds = list_of_seeds.split(',')
-        list_of_seeds = [int(s) for s in list_of_seeds]
+        list_of_seeds = [int(s) for s in list_of_seeds if s]
    
-    
+    print(list_of_seeds)    
 
     
     if language_model_type=='all':
@@ -101,8 +103,10 @@ def run_experiment(language_model_type='all',running_mode='default', task='all',
 
     elif language_model_type=='large':
         models_to_be_used = models_to_be_used_large
+    else:
+        models_to_be_used = [language_model_type]
 
-    models_to_be_used = list(set(models_to_be_used))
+    models_to_be_used = sorted(list(set(models_to_be_used)))
     print(models_to_be_used)
 
     commands_already_satisfied = list()
@@ -115,6 +119,7 @@ def run_experiment(language_model_type='all',running_mode='default', task='all',
         all_variables_perturbations = ['$'.join([str(x) for x in p]) for p in all_variables_perturbations]        
         all_variables_perturbations = list(zip(cycle(gpu_number), all_variables_perturbations)) 
         all_variables_perturbations = [[x[0]]+x[1].split('$') for x in all_variables_perturbations]
+        print(all_variables_perturbations)
              
         for (gpu_id,task,model_name,seed) in all_variables_perturbations:
             if task in ["run_greek_legal_ner", "run_mapa_ner_fine_grained", "run_mapa_ner_coarse_grained", "run_legalnero", "run_lener_br"]:
@@ -128,6 +133,7 @@ def run_experiment(language_model_type='all',running_mode='default', task='all',
             script_new = generate_command(gpu_number=gpu_id,model_name=model_name,lower_case=lower_case,task=task,seed=seed,num_train_epochs=num_train_epochs,batch_size=batch_size,accumulation_steps=accumulation_steps,language=language,running_mode=running_mode,learning_rate=learning_rate,code=task_code_mapping[task],metric_for_best_model=metric_for_best_model)
             if script_new is not None:
                 command = 'bash '+str(script_new)
+                print(command)
                 if command not in commands_to_run:
                     if len(commands_to_run) <= number_of_parallel_commands:
                         commands_to_run.append(command)
@@ -156,6 +162,7 @@ def run_experiment(language_model_type='all',running_mode='default', task='all',
             script_new = generate_command(gpu_number=gpu_id,model_name=model_name,lower_case=lower_case,task=task,seed=seed,num_train_epochs=num_train_epochs,batch_size=batch_size,accumulation_steps=accumulation_steps,language=language,running_mode=running_mode,learning_rate=learning_rate,code=task_code_mapping[task],metric_for_best_model=metric_for_best_model)
             if script_new is not None:
                 command = 'bash '+str(script_new)
+                print(command)
                 if command not in commands_to_run:
                     if len(commands_to_run) <= number_of_parallel_commands:
                         commands_to_run.append(command)
@@ -166,7 +173,9 @@ def run_experiment(language_model_type='all',running_mode='default', task='all',
                         commands_to_run.append(command)
                         commands_already_satisfied.append((model_name,task,seed))
             
-    
+    if len(all_commands_to_run)==0:
+        all_commands_to_run.append(commands_to_run)
+
     command_dict= dict()
     for n,c in enumerate(all_commands_to_run):
         command_dict[n]=c
@@ -176,8 +185,7 @@ def run_experiment(language_model_type='all',running_mode='default', task='all',
 
             
             
-    for commands_to_run in all_commands_to_run:    
-        print(commands_to_run)
+    for commands_to_run in all_commands_to_run:
         run_in_parallel(commands_to_run)
 
    
@@ -203,7 +211,7 @@ if __name__=='__main__':
     
     parser.add_argument('-lmt','--language_model_type', help='Define which kind of language model you would like to use; you can choose between small, base and large language models or all of them.', default='all')
     parser.add_argument('-rmo','--running_mode', help='Define whether you want to run the finetungin on all available training data or just a small portion for testing purposes.', default='default')
-    parser.add_argument('-t','--task', help='Choose a task.', default='all',choices=list(task_code_mapping.keys()))
+    parser.add_argument('-t','--task', help='Choose a task.', default='all',choices=sorted(list(task_code_mapping.keys())))
     parser.add_argument('-bz','--batch_size', help='Define the batch size.', default=10)
     parser.add_argument('-nte','--num_train_epochs', help='Define the number of training epochs.', default=20)
     parser.add_argument('-los','--list_of_seeds', help='Define the number of training epochs.', type=str)
