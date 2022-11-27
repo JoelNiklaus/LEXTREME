8082bf93ff201a07f09a901204fe3d183baa9f04
kapllan <veton-matoshi@hotmail.de>
2022-11-27 18:44:49 +0100

    Added script to generate aggregated scores

--- utils/create_overview.py
+++ utils/create_overview.py
@@ -1,44 +1,139 @@
 from pathlib import Path
 import json as js
 import pandas as pd
+from statistics import mean
+import argparse
+import sys
+from datetime import datetime
+import logging
 
 
 
-def merge_dicts(*dict_args):
-    """
-    Given any number of dictionaries, shallow copy and merge into a new dict,
-    precedence goes to key-value pairs in latter dictionaries.
-    """
-    result = {}
-    for dictionary in dict_args:
-        result.update(dictionary)
-    return result
+
+class ResultAggregator:
+
+
+    def __init__(self, path_to_csv_export):
+        
+
+        name_of_log_file = 'logs/loggings_'+datetime.now().isoformat()+'.txt'
+
+        logging.basicConfig(level=logging.DEBUG, filename=name_of_log_file, filemode="a+",format="%(asctime)-15s %(levelname)-8s %(message)s")
+
+        self._path = name_of_log_file
+        results = pd.read_csv(Path(path_to_csv_export))
+        results = results[results.finetuning_task.isnull()==False]
+        self.results = results[results.State=="finished"]
+
+    def __enter__(self):
+        sys.stdout = open(self._path, mode="w")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = sys.__stdout__
+
+    def get_info(self): 
+        logging.info("The current export contains results for the following tasks:")
+        logging.info(self.results.finetuning_task.unique())
+
+
+    def get_average_score(self, finetuning_task:str, _name_or_path:str, score:str='predict/_macro-f1') -> float:
+    
+        results_filtered = self.results[(self.results.finetuning_task==finetuning_task) & (self.results._name_or_path==_name_or_path)][["seed",score]]
+        
+        if len(results_filtered.seed.unique()) != len(results_filtered.seed.tolist()):
+            logging.info('Attention! It seems you have duplicate seeds for task '+finetuning_task+' with the model '+_name_or_path)
+            logging.info('Duplicate values will be removed based on the seed number.')
+            
+            results_filtered.drop_duplicates('seed', inplace=True)
+        
+        return results_filtered[score].mean()
+
+
+    def create_template(self):
+        # Create empty dataframe
+        overview_template = pd.DataFrame()
+
+        # Se the list of model names as index
+        overview_template = overview_template.set_axis(self.results._name_or_path.unique())
+
+        # Name the index axis as Model
+        overview_template.index.names = ['Model']
+
+        for finetuning_task in self.results.finetuning_task.unique():
+            overview_template[finetuning_task]=''
+
+        self.overview_template = overview_template
+
+
+    def insert_dataset_average_scores(self):
+
+        logging.info("*** Calculating average scores ***")
+        for _name_or_path in self.results._name_or_path.unique():
+            for finetuning_task in self.results.finetuning_task.unique():
+                mean_macro_f1_score = self.get_average_score(finetuning_task, _name_or_path)
+                self.overview_template.at[_name_or_path,finetuning_task]=mean_macro_f1_score
+
+
+        if self.overview_template.isnull().values.any():
+            logging.info('Attention! For some cases we do not have an aggregated score! These cases will be converted to zero.')
+            self.overview_template.fillna(0.0, inplace=True)
+
+
+    def get_dataset_aggregated_score(self):
+
+        # Insert aggregated mean for each model name
+
+        self.create_template()
+        self.insert_dataset_average_scores()
+
+        self.overview_template['dataset_aggregate_score']=''
+
+        for _name_or_path in self.overview_template.index.tolist():
+            all_mean_macro_f1_scores =  self.overview_template.loc[_name_or_path].tolist()
+            all_mean_macro_f1_scores_cleaned = [x for x in all_mean_macro_f1_scores if type(x)!=str]
+            if all_mean_macro_f1_scores_cleaned!=all_mean_macro_f1_scores:
+                logging.info('Attention! There are string values for your score!')
+                
+            all_mean_macro_f1_scores_mean = mean(all_mean_macro_f1_scores_cleaned)
+            self.overview_template.at[_name_or_path,'dataset_aggregate_score']=all_mean_macro_f1_scores_mean
+
+        columns = self.overview_template.columns.tolist()
+        first_columns = ['dataset_aggregate_score']
+        self.overview_template = self.overview_template[first_columns+[col for col in columns if col not in first_columns]]
+        self.overview_template.to_csv('dataset_aggregated_scores.csv')
+
+
+    
+
+
+        
+
+
+        
+        
 
 
 
-log_path = Path('../scripts/logs/')
-
-#directories = [x[0] for x in os.walk(log_path)]
-directories = [f for f in log_path.glob('**/*') if f.is_dir()]
-print(directories)
-
-overview = list()
-
-for d in directories:
-    all_files = [f for f in d.glob('**/*') if f.is_file()]
-    for f in all_files:
-        if str(f).endswith('all_results.json'):
-            with open(f,'r') as f:
-                all_results = js.load(f)
-        if str(f).endswith('config.json'):
-            with open(f,'r') as f:
-                config = js.load(f)
-            all_results = merge_dicts(all_results, config)
-            overview.append(all_results)
 
 
-overview_df = pd.DataFrame(overview)
-columns_without_finetuning_task = [c for c in overview_df.columns.tolist() if c !='finetuning_task']
-overview_df = overview_df[['finetuning_task']+columns_without_finetuning_task]
-print(overview_df)
-overview_df.to_csv('../overview_of_results.csv',index=False)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-pce', '--path_to_csv_export', help='Insert the path to the exported csv file from wandb')
+
+    args = parser.parse_args()
+
+    ra = ResultAggregator(args.path_to_csv_export)
+
+    ra.get_info()
+
+    ra.get_dataset_aggregated_score()
+
+    
+
+
+
