8739c4d51cbb851323dbf9ea7035036a6aa831e6
kapllan <veton-matoshi@hotmail.de>
2023-05-08 19:58:51 +0200

    Changed the scripts to run LexGlue experiments

--- scripts/experiments_for_lexglue.sh
+++ scripts/experiments_for_lexglue.sh
@@ -1,9 +1,11 @@
-GPU_NUMBER=X
-GPU_MEMORY=X
+GPU_NUMBER=0
+GPU_MEMORY=80
 
 cd ..
 
-for finetuning_task in [ecthr_a,ecthr_b, eurlex,scotus,ledgar,unfair_tos, case_hold]
+
+for ft in ecthr_a ecthr_b eurlex scotus ledgar unfair_tos case_hold
 	do
-		python main.py -gn {GPU_NUMBER} -gm {GPU_MEMORY} -t finetuning_task -los 1,2,3,4,5 --do_lower_case -bz 8 -mfbm micro-f1 -nte 20 -lr 3e-5 -esp 3
+	echo $ft
+	python main.py -gn {GPU_NUMBER} -gm {GPU_MEMORY} -t $ft -los 1,2,3,4,5 --lower_case true -bz 8 -mfbm micro-f1 -nte 20 -lr 3e-5 -esp 3 -ld paper_results
 	done
