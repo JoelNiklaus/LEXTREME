c8368b178c44dd33b36176dc486983a07ae00d98
Veton Matoshi <vetonmatoshi@MacBook-Pro-von-Veton.local>
2023-05-08 20:15:38 +0200

    Added a python script to run the LexGlue experiments

--- scripts/experiments_for_lexglue.py
+++ scripts/experiments_for_lexglue.py
@@ -22,5 +22,5 @@ for row in models.to_dict(orient="records"):
     name_or_path = row["_name_or_path"]
     revision = row["revision"]
     for ft in tasks:
-        command = 'python main.py -gn 0 -gm 80 -los 1,2,3,4,5 --lower_case true -bz 8 -mfbm micro-f1 -nte 20 -lr 3e-5 -esp 3 -ld paper_results ' + '-t '+ft
+        command = 'python main.py -gn 0 -gm 80 -los 1,2,3,4,5 --lower_case true -bz 8 -mfbm micro-f1 -nte 20 -lr 3e-5 -esp 3 -ld paper_results ' + '-t '+ft +' -lmt '+ name_or_path +' -rev '+revision
         os.system(command)
