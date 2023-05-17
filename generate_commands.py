f3edf624e0731a5b00028a345f35a55a0ffc1e35
kapllan <veton-matoshi@hotmail.de>
2023-05-17 18:08:33 +0200

    Updated the code to generate commands

--- generate_commands.py
+++ generate_commands.py
@@ -3,17 +3,18 @@ import argparse
 import os
 
 
-df = pd.read_excel('utils/results/report.xlsx')
-df = df[df.finetuning_task.str.contains("multi_eur")]
-df = df[df._name_or_path.str.contains("(joelito|roberta)")==False]
+df = pd.read_excel('utils/results/report_apex.xlsx')
+df = df[df.run == "x"]
+# idf = df[df._name_or_path.str.contains("(joelito|roberta)")==False]
 
 
-
-def generate_commands(gm,gn,ld):
-    commands = list()    
+def generate_commands(gm, gn, ld):
+    commands = list()
     for r in df.to_dict(orient="records"):
 
-        comm = 'python main.py -gm '+str(gm)+' -gn '+str(gn) +' -t '+r["finetuning_task"]+' -lmt '+r["_name_or_path"]+' -los '+r['missing_seeds']+' -ld '+ld+' --revision '+r['revision']
+        comm = 'python main.py -gm '+str(gm)+' -gn '+str(gn) + ' -t '+r["finetuning_task"]+' -lmt ' + \
+            r["_name_or_path"]+' -los '+r['missing_seeds'] + \
+            ' -ld '+ld+' --revision '+r['revision']
         commands.append(comm)
 
     return ' ; sleep 10 ; '.join(commands)
@@ -28,9 +29,7 @@ if __name__ == '__main__':
     parser.add_argument('-ld')
 
     args = parser.parse_args()
-    
+
     commands = generate_commands(args.gm, args.gn, args.ld)
 
     os.system(commands)
-
-
