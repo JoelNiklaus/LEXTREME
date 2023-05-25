a9fc64ba35db0def36a79655ecd3bd1471219433
kapllan <veton-matoshi@hotmail.de>
2023-05-25 09:05:54 +0200

    Saved the script

--- scripts/experiments_for_lexglue.py
+++ scripts/experiments_for_lexglue.py
@@ -1,25 +1,20 @@
 import pandas as pd
 import os
 
-path_to_file = os.path.join(os.path.dirname(__file__), '../utils/joels_models_revision_infos_MultiLegalPile.xlsx')
-models = pd.read_excel(path_to_file)
 
-models = models[models.need_to_be_run_with_LexGlue == True]._name_or_path.tolist()
 
 tasks = [
     "eurlex",
     "scotus",
     "ledgar",
     "unfair_tos",
-    "case_hold",
     "ecthr_a",
     "ecthr_b"
 ]
 
 missing_runs = pd.read_excel('../utils/results/lextreme/paper_results/report.xlsx', sheet_name='completeness_report')
 missing_runs = missing_runs[missing_runs.finetuning_task.isin(tasks)]
-missing_runs = missing_runs[missing_runs._name_or_path.isin(models)]
-missing_runs = missing_runs[missing_runs._name_or_path.str.contains('joelito.*longfo')]
+missing_runs = missing_runs[missing_runs._name_or_path.str.contains('joelito.*large')]
 
 print(missing_runs.shape)
 print(missing_runs.head())
@@ -31,5 +26,5 @@ for row in missing_runs.to_dict(orient="records"):
     revision = row["revision"]
     ft = row['finetuning_task']
     seeds = row['missing_seeds']
-    command = 'python main.py -gn 1 -gm 80 -los ' + seeds + ' --lower_case true -bz 8 -mfbm micro-f1 -nte 20 -lr 3e-5 -esp 3 -ld paper_results ' + '-t ' + ft + ' -lmt ' + name_or_path + ' -rev ' + revision
+    command = 'python main.py -gn 0 -gm 80 -los ' + seeds + ' --lower_case true -bz 4 -as 2 -mfbm micro-f1 -nte 20 -lr 3e-5 -esp 3 -ld paper_results ' + '-t ' + ft + ' -lmt ' + name_or_path + ' -rev ' + revision
     os.system(command)
