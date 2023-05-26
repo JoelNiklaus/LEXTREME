09dbdd16cb3e8b330cd4365f86d7035bba8d8638
kapllan <veton-matoshi@hotmail.de>
2023-05-26 13:56:01 +0200

    Interim commit

--- scripts/experiments_for_lexglue.py
+++ scripts/experiments_for_lexglue.py
@@ -4,11 +4,6 @@ import os
 
 
 tasks = [
-    "eurlex",
-    "scotus",
-    "ledgar",
-    "unfair_tos",
-    "ecthr_a",
     "ecthr_b"
 ]
 
@@ -26,5 +21,5 @@ for row in missing_runs.to_dict(orient="records"):
     revision = row["revision"]
     ft = row['finetuning_task']
     seeds = row['missing_seeds']
-    command = 'python main.py -gn 0 -gm 80 -los ' + seeds + ' --lower_case true -bz 4 -as 2 -mfbm micro-f1 -nte 20 -lr 3e-5 -esp 3 -ld paper_results ' + '-t ' + ft + ' -lmt ' + name_or_path + ' -rev ' + revision
+    command = 'python main.py -gn 0,1 -gm 80 -los ' + seeds + ' --lower_case true -bz 4 -as 2 -mfbm micro-f1 -nte 20 -lr 3e-5 -esp 3 -ld paper_results ' + '-t ' + ft + ' -lmt ' + name_or_path + ' -rev ' + revision
     os.system(command)
