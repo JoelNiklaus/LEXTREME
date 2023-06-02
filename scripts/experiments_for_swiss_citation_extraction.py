55ac974bf3ecd63368a94ed1e21f8c47aa4002c9
kapllan <veton-matoshi@hotmail.de>
2023-06-02 19:19:16 +0200

    Fixing a bug

--- scripts/experiments_for_swiss_citation_extraction.py
+++ scripts/experiments_for_swiss_citation_extraction.py
@@ -1,8 +1,9 @@
 import json as js
 import os
-
+from pprint import pprint
 with open('../utils/report_specs/swiss_citation_extraction.json', 'r') as f:
     report_specs = js.load(f)
+    pprint(report_specs)
 
 required_models = [
     "microsoft/Multilingual-MiniLM-L12-H384",
@@ -26,6 +27,6 @@ for model_and_revision in report_specs["_name_or_path"]:
         ft = 'original'
         seeds = '1,2,3'
         command = 'python main.py -gn 0,1 -gm 80 -los ' + seeds + \
-                  '-ld swiss_citation_extraction ' + \
-                  '-t ' + ft + ' -lmt ' + name_or_path + ' -rev ' + revision
+                  ' -ld swiss_citation_extraction ' + \
+                  ' -t ' + ft + ' -lmt ' + name_or_path + ' -rev ' + revision
         os.system(command)
